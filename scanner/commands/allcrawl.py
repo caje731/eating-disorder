214cdf379fc08c0087f03925c5bb898354b5411d
Cajetan Rodrigues <cajetan@cajetan-Latitude-3540.(none)>
2015-01-02 22:19:45 +0530

    Changed the hard-coded path pointing to distribute.py

--- scanner/commands/allcrawl.py
+++ scanner/commands/allcrawl.py
@@ -8,14 +8,12 @@ class AllCrawlCommand(ScrapyCommand):
 		return "Schedule a run for all available spiders"
 		
 	def run(self, args, opts):
-		crawler 		= self.crawler_process.create_crawler(self.crawler_process.settings)
+		crawler 	= self.crawler_process.create_crawler(self.crawler_process.settings)
 		spider_names 	= crawler.spiders.list()
 		for spider in spider_names:
 			args.append('spider='+spider)
 		#dist_response = os.system('python "C:\Git Repos\MassBlurb\scanner\util\distribute.py" '+ ' '.join(args))
-		subprocess.call(['python', "C:\GitRepos\MassBlurb\scanner\util\distribute.py"] + args)
-		
-		#subprocess.check_output('python "C:\Git Repos\MassBlurb\scanner\util\distribute.py" ' + ' '.join(args), shell=True)
+		subprocess.call(['python', os.getcwd() + "/distribute.py"] + args)
 
 if __name__ == "__main__":
 	print 	'Usage: scrapy allcrawl arg1=value1 arg2=value2 ...'
